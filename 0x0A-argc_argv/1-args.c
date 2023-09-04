8909ae451070a8e07e649c5edfccda95ee4880df
Your Name <tomsarfodavis@gmail.com>
2023-09-04 01:27:22 -0700

    Write a program that prints the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints number of argument passed.
+ * @argc: number of command line argument.
+ * @argv: array of command line argument.
+ * Return:0  success.
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
