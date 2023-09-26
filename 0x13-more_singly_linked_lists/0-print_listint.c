05ed03aa8c39343fcdbde801e4f2b5ac3b8d21d1
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-09-26 04:22:43 -0700

    Write a function that returns the elements of a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
+#include"lists.h"
 /**
- * print_listint - print elements of list
- * @h: ponter
- * Return: integer
- * Description: prints out all the elements of a list
- *
- */
-#include "lists.h"
-
+  *print_listint - prints all elements
+  *@h: the single linked list
+  *Return: an integer
+  */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			count++;
+			h = h->next;
+		}
 	}
 	return (count);
 }
