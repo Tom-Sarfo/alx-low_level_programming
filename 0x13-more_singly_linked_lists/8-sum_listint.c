59fe940cc3e22488da89a7358492a09d6ee55fa6
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-09-26 12:27:45 -0700

    Return the sum of n in a linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+/**
+ * sum_listint - returns the sum of n
+ * @head: pointer to the first node
+ * Return: integer - sum of n of all nodes
+ * Description: returns the sum of n data of a linked list
+ */
+#include "lists.h"
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum = 0;
+	temp = head;
+
+	while (temp != NULL)
+	{
+		sum = sum + temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
